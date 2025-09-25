6b8d3e0b13f2a06cdb4e7aff2faa80ba8514cfa4
DirsanA <dirsannsr079814@students.wku.edu.et>
2025-09-25 11:52:46 -0700

    guessing number from input

---
+++ GuessNumber/app.py
@@ -0,0 +1,15 @@
+import random
+
+def randum_num():
+    num=random.randint(100)
+    while True:
+        random_input=input("Please Enter number between 1 and 100: ")
+        if num!=random_input:
+            if num>random_input:
+                print("It is greater number you enter : ")
+            elif num<random_input:
+                print("It is smaller number that you enter: ")
+        else:
+            print("Congratulation You have succesfuly got the number ",num)
+        
+randum_num()
