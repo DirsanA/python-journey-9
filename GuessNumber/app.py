f7e5c0c55c7e2d9b39db9e83f4e9fe0fcbc2245a
DirsanA <dirsannsr079814@students.wku.edu.et>
2025-10-02 00:55:10 -0700

    guessing number in python

--- GuessNumber/app.py
+++ GuessNumber/app.py
@@ -1,15 +1,21 @@
+
 import random
 
-def randum_num():
-    num=random.randint(1,100)
+target_number=random.randint(1,100)
+def guess_number():
     while True:
-        random_input=input("Please Enter number between 1 and 100: ")
-        if num!=random_input:
-            if num>random_input:
-                print("It is greater number you enter : ")
-            elif num<random_input:
+        try:
+            guess_number=int(input("please enter the number between 1 and 100: "))
+            if guess_number<1 or guess_number>100:
+                print("please enter the number between 1 and 100: ")
+            elif guess_number==target_number:
+                print("Congratulation You have succesfuly got the number ",guess_number)
+                break
+            elif guess_number<target_number:
                 print("It is smaller number that you enter: ")
-        else:
-            print("Congratulation You have succesfuly got the number ",num)
-        
-randum_num()
+            elif guess_number>target_number:
+                print("It is greater number you enter: ")
+        except ValueError:
+            print("Please enter correct number: ")
+guess_number()
+
