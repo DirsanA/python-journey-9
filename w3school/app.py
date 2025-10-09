33b9b1585b6c1428943ed4579c6565b1d1c4a7ef
DirsanA <dirsannsr079814@students.wku.edu.et>
2025-10-09 05:41:54 -0700

    copying list uisng copy function

--- w3school/app.py
+++ w3school/app.py
@@ -1,6 +1,56 @@
-list_name=["abebe","kebede","seyioum"]
-list_name.append("abirham")
-print(list_name)
-print(list_name.pop(-1))
-print("after poping the list! ")
-print(list_name)
+# # list it allows duplicate entery and also it is orderd // another which is indexed and can be changed
+
+# list_name=["abebe","kebede","seyioum"]
+# list_name.append("abirham")
+# print(list_name)
+# print(list_name.pop(-1))
+# print("after poping the list! ")
+# print(list_name)
+
+# #  inserting at last postion of list 
+# # by using insert(index,value ) it takes two value index and value of list 
+
+# list_name.insert(-1,"ban")
+
+# # print after adding list item at last of list 
+# print(list_name)
+
+# removing list member in python from list member
+
+# list_items=["abro","banku","shimeles"]
+
+# list_items.remove(list_items[-1])
+
+# print(list_items)
+
+# we can also del the list 
+
+# list_item=[1,2,3]
+
+# print(list_item)
+
+# [print(i) for i in list_item]
+
+
+# name_list=["abebe","kebede","almaz"]
+
+# new_list=[]
+
+
+# for x in name_list:
+#     if "a" in x:
+#         name_list.append(x)
+
+# print(new_list)
+
+# for name in name_list:
+#     name_list=name.upper()
+    
+# print(name_list)
+    
+
+    
+# name_list=["dirsan","samuel","cherent"]
+
+# other_list=name_list.copy()
+# print(other_list)
