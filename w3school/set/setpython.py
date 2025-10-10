b1b6335fed18ba67ef9958f0254d2e9c72a03cf3
DirsanA <dirsannsr079814@students.wku.edu.et>
2025-10-10 03:47:20 -0700

    union set using python

--- w3school/set/setpython.py
+++ w3school/set/setpython.py
@@ -1,10 +1,25 @@
 # set is un ordered 
 #  which is also do not allow duplicate value 
 
-set_item={"alemu","kebede","abebe"}
+# set_item={"alemu","kebede","abebe"}
 
-# print(len(set_item))
+# # print(len(set_item))
 
-set_item.add("abre")
+# for i in set_item:
+#     if "d" in i:
+#         print("exist")
+#         break
+#     else:
+#         print("does not exist")
+#         break
 
-print(set_item)
+
+# join set 
+
+set1={"list1","list2","list3"}
+
+set2={"abriham","addd"}
+
+set3=set1.union(set2)
+
+print(set3)
